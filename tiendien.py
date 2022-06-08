f92d65b319038706d9b05f86fa9b35a647d6d397
thanh nhat <dtthuhienqng@gmail.com>
2022-06-08 19:25:15 +0700

    cap nhat file tièn dien

--- tiendien.py
+++ tiendien.py
@@ -8,13 +8,17 @@ tien = kwhmoi - kwhcu
 if tien < 50:
     tongtien = tien * 1.678
 elif tien < 100:
-    tongtien = tien * 1.734
+    tongtien = 50 * 1.678 + (tien - 50)  * 1.734 
+    print(f"Tổng số tiền của bạn là {tongtien}")
 elif tien < 200:
-    tongtien = tien * 2.014
+    tongtien = 50 * 1.678 + (tien - 50)  * 1.734 + (tien - 100) * 2.014
+    print(f"Tổng số tiền của bạn là {tongtien}")
 elif tien < 300:
-    tongtien = tien * 2.536
+    tongtien = 50 * 1.678 + (tien - 50)  * 1.734 + (tien - 100) * 2.014 + (tien - 200) * 2.536
+    print(f"Tổng số tiền của bạn là {tongtien}")
 elif tien < 400:
-    tongtien = tien * 2.834
-else: 
-    tongtien = tien * 2.927
+    tongtien = 50 * 1.678 + (tien - 50)  * 1.734 + (tien - 100) * 2.014 + (tien - 200) * 2.536 + (tien - 300) * 2.834
+    print(f"Tổng số tiền của bạn là {tongtien}")
+else:
+    tongtien = 50 * 1.678 + (tien - 50)  * 1.734 + (tien - 100) * 2.014 + (tien - 200) * 2.536 + (tien - 300) * 2.834 + (tien - 400) * 2.934
     print(f"Tổng số tiền của bạn là {tongtien}")
